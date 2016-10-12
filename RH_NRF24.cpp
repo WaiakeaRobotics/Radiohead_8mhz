967311061a149aaa0a38f74934e4bfad306a3144
Chester <hilo90mhz@gmail.com>
2016-10-11 22:41:04 -1000

    Change SPI clock to 8mhz
    This change is necessary to fix IR LED output problems.

--- RH_NRF24.cpp
+++ RH_NRF24.cpp
@@ -18,7 +18,7 @@ bool RH_NRF24::init()
 {
     // Teensy with nRF24 is unreliable at 8MHz:
     // so is Arduino with RF73
-    _spi.setFrequency(RHGenericSPI::Frequency1MHz);
+    _spi.setFrequency(RHGenericSPI::Frequency8MHz);
     if (!RHNRFSPIDriver::init())
 	return false;
 
